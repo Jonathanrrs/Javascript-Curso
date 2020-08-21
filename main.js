95089175628ba9aa251fc061bc464b8c2587fed4
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-08-20 19:17:23 -0500

    number

--- main.js
+++ main.js
@@ -9,3 +9,36 @@
 /* valua si el primer es falso se queda con ese */
 // let d = null && a;
 
+/* NÚMEROS */
+
+/* toFixed */
+/* Especifica el número de decimales, hace redondeo */
+// number.toFixed();
+
+/* parseInt - parseFloat */
+/* Convierte texto a número */
+// parseInt('var, 10) 10 por el sistema decimal
+/* Convertir a número con decimales */
+// parseFloat(var)
+
+/* NaN */
+/* Cualquier operación matemática que no devuelva un número */
+// let number = 5 * 'hola';
+
+/* Math */
+
+/* Math.floor */
+/* Redondear un número */
+// Math.floor(20.7) -> 20, lo redondea hacia abajo
+
+/* Math.ceil */
+/* Redondear un número hacia arriba */
+// Math.ceil(20.1) -> 21
+
+/* Math.round */
+/* Redondea de .5 para arriba, de .5 para abajo redondea hacia abajo */
+// Math.round(20.6) -> 21
+
+/* Math.random */
+/* Número aleatorio entre 0 y 1 */
+// Math.random() * 10
