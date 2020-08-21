09aee5df4c1872720cfa6887d4f7a4cdda9ba5b8
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-08-20 19:10:14 -0500

    operadores

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Operador ternario */
+/* ? como if, : como else */
+// let nombre = 'joni';
+// nombre == 'joni' ? console.log(nombre) : console.log('no coincide');
+
+/* Operador corto circuit */
+/* evalua el primer valor y si es falso se pasa al otro*/
+// let c = null || 25;
+/* valua si el primer es falso se queda con ese */
+// let d = null && a;
+
